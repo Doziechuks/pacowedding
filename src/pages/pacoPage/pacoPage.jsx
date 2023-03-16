b56f7e8093609f378279fbcd199e3e5321428e50
doziechuks <doziechuks1010@gmail.com>
2023-03-16 16:10:54 +0100

    updated the pacoPage component

--- src/pages/pacoPage/pacoPage.jsx
+++ src/pages/pacoPage/pacoPage.jsx
@@ -24,21 +24,29 @@ const PacoPage = () => {
   console.log(attendees);
   return (
     <section className={classes.wrapper}>
-      <h1>expected guests</h1>
-      <div className={classes.container}>
-        <div className={classes.heading}>
-          <h2 className={classes.serialNum}>s/n</h2>
-          <h2 className={classes.name}>name</h2>
-          <h2 className={classes.phone}>phone</h2>
-          <h2 className={classes.checked}>present</h2>
+      <h1>expected guests</h1>{" "}
+      {attendees.length - 1 === 0 ? (
+        <div className={classes.noInterest}>no interest indicated yet</div>
+      ) : (
+        <div className={classes.container}>
+          <div className={classes.heading}>
+            <h2 className={classes.serialNum}>s/n</h2>
+            <h2 className={classes.name}>name</h2>
+            <h2 className={classes.phone}>phone</h2>
+            <h2 className={classes.checked}>present</h2>
+          </div>
+          {attendees.map((data, index) => {
+            let serialNumber = index + 1;
+            return (
+              <TableInfo
+                key={data.id}
+                data={data}
+                serialNumber={serialNumber}
+              />
+            );
+          })}
         </div>
-        {attendees.map((data, index) => {
-          let serialNumber = index + 1;
-          return (
-            <TableInfo key={data.id} data={data} serialNumber={serialNumber} />
-          );
-        })}
-      </div>
+      )}
     </section>
   );
 }
